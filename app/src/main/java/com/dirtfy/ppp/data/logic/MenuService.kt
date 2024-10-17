c140813a11869397773425e54d228f6d809161f0
retomoo <retomoo@naver.com>
2024-10-18 01:54:47 +0900

    [Issue #11] Elimination of FlowState from return type of functions of MenuService

--- app/src/main/java/com/dirtfy/ppp/data/logic/MenuService.kt
+++ app/src/main/java/com/dirtfy/ppp/data/logic/MenuService.kt
@@ -3,27 +3,30 @@ package com.dirtfy.ppp.data.logic
 import com.dirtfy.ppp.common.exception.MenuException
 import com.dirtfy.ppp.data.dto.DataMenu
 import com.dirtfy.ppp.data.source.repository.MenuRepository
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class MenuService @Inject constructor(
     private val menuRepository: MenuRepository
 ): Service {
 
-    fun createMenu(menu: DataMenu) = asFlow {
+    fun createMenu(menu: DataMenu) = flow {
         menuRepository.let {
             if (it.isSameNameExist(menu.name))
                 throw MenuException.NonUniqueName()
 
-            it.create(menu)
+            emit(it.create(menu))
         }
     }
 
-    fun readMenu() = asFlow {
-        menuRepository.readAll()
+    fun readMenu() = flow {
+        val menuList = menuRepository.readAll()
+        emit(menuList)
     }
 
-    fun deleteMenu(menu: DataMenu) = asFlow {
-        menuRepository.delete(menu)
+    fun deleteMenu(menu: DataMenu) = flow {
+        val deletedMenu = menuRepository.delete(menu)
+        emit(deletedMenu)
     }
 
     fun menuStream() = menuRepository.menuStream()
