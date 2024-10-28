e5f01c60949cd6caa0c9c60c78657103d2cb0db3
retomoo <retomoo@naver.com>
2024-10-29 00:08:49 +0900

    build normalized and unnecessary codes removed

--- app/src/main/java/com/dirtfy/ppp/common/di/DataModule.kt
+++ app/src/main/java/com/dirtfy/ppp/common/di/DataModule.kt
@@ -8,13 +8,8 @@ import com.dirtfy.ppp.data.api.impl.feature.account.firebase.AccountFireStore
 import com.dirtfy.ppp.data.api.impl.feature.menu.firebase.MenuFireStore
 import com.dirtfy.ppp.data.api.impl.feature.record.firebase.RecordFireStore
 import com.dirtfy.ppp.data.api.impl.feature.table.firebase.TableFireStore
-import com.dirtfy.ppp.data.logic.AccountBusinessLogic
-import com.dirtfy.ppp.data.logic.MenuBusinessLogic
-import com.dirtfy.ppp.data.logic.RecordBusinessLogic
-import com.dirtfy.ppp.data.logic.TableBusinessLogic
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 
@@ -42,36 +37,5 @@ abstract class DataModule {
         tableImplementation: TableFireStore
     ): TableApi
 
-    companion object {
 
-        @Provides
-        fun providesAccountBusinessLogic(
-            accountApi: AccountApi
-        ): AccountBusinessLogic {
-            return AccountBusinessLogic(accountApi)
-        }
-
-        @Provides
-        fun providesMenuBusinessLogic(
-            menuApi: MenuApi
-        ): MenuBusinessLogic {
-            return MenuBusinessLogic(menuApi)
-        }
-
-        @Provides
-        fun providesRecordBusinessLogic(
-            recordApi: RecordApi
-        ): RecordBusinessLogic {
-            return RecordBusinessLogic(recordApi)
-        }
-
-        @Provides
-        fun providesTableBusinessLogic(
-            tableApi: TableApi,
-            recordApi: RecordApi
-        ): TableBusinessLogic {
-            return TableBusinessLogic(tableApi, recordApi)
-        }
-
-    }
 }
